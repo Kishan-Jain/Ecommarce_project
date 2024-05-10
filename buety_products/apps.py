43560e8d8c2a70e7dd4d645dd82950e390dabc4a
Kishan Jain <jainshab7000@gmail.com>
2024-05-10 11:33:55 +0530

    date-10/05/2024

---
+++ buety_products/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BuetyProductsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'buety_products'
---
+++ daily_uses/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DailyUsesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'daily_uses'
